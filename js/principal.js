b0d6c16fc5bb8eaafda8ccdca021698443c12a29
Mikael <mk.mikael159@gmail.com>
2022-05-28 10:54:55 -0300

    adiciona mais caracteres permitidos

--- js/principal.js
+++ js/principal.js
@@ -9,7 +9,7 @@ var mensagem = document.querySelector(".mensagem");
 var erros;
 var mostraErros = document.querySelector(".erros");
 var novo = document.querySelector('.novojogo');
-var alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç', 'á', 'é', 'í', 'ó', 'ú', 'à', 'ã', 'õ'];
+var alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç', 'á', 'é', 'í', 'ó', 'ú', 'à', 'ã', 'õ', 'â', 'ê', 'ô'];
 // FUNÇÃO QUE SORTEIA AS PALAVRAS DO ARRAY "PALAVRAS" E CRIA OUTRO ARRAY COM AS LETRAS DA PALAVRA SORTEADA
 function sorteia() {
     
